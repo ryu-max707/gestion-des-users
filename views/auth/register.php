f9c8598348fdbd699dab567f48152a7a1bb0d999
unknown <mbockryu@gmail.com>
2025-03-24 12:15:59 +0100

    message d'erreur

--- views/auth/register.php
+++ views/auth/register.php
@@ -1,3 +1,18 @@
+<?php if (isset($_SESSION['error_message'])) : ?>
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4">
+        <?= $_SESSION['error_message']; ?>
+    </div>
+    <?php unset($_SESSION['error_message']); // Supprime le message après affichage ?>
+<?php endif; ?>
+
+<?php if (isset($_SESSION['success_message'])) : ?>
+    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
+        <?= $_SESSION['success_message']; ?>
+    </div>
+    <?php unset($_SESSION['success_message']); // Supprime le message après affichage ?>
+<?php endif; ?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
